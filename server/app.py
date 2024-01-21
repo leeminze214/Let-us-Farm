e8c668810250a72b066bc0c022f5dc2da24982e0
Lee Zheng <leeminz214@gmail.com>
2024-01-20 21:00:54 -0500

    commands

--- server/app.py
+++ server/app.py
@@ -1,27 +1,48 @@
-from flask import Flask,render_template, request
+from flask import Flask,render_template, request, redirect
 import requests
 
 app = Flask(__name__)
 
+#constants
+M_PER_S = 0.1
+IMG_X_SIZE = 100
+IMG_Y_SIZE= 100
+FIELD_X_SIZE=5
+FIELD_Y_SIZE=5
+ESP_IP = ""
 
 @app.route('/')
 def home():
     return render_template("home.html")
 
+@app.route('/forward')
+def forward():
+    requests.post(ESP_IP,data= "forward")
+    return 0
+
+@app.route('/back')
+def back():
+    requests.post(ESP_IP,data= "back")
+    return 0
+
+@app.route('/left_t')
+def left_t():
+    requests.post(ESP_IP,data= "left_t")
+    return 0
+
+@app.route('/right_t')
+def right_t():
+    requests.post(ESP_IP,data= "right_t")
+    return 0
+
 @app.route('/new_coord')
 def move_to_position():
-    m_per_s = 0.1
-    img_x_size = 100
-    img_y_size= 100
-    field_x_size=5
-    field_y_size=5
     pixel = request.query_string
     x_pixel,y_pixel= pixel.split(",")
-    x_time = field_x_size*(x_pixel/img_x_size)/m_per_s
-    y_time = field_y_size*(y_pixel/img_y_size)/m_per_s
-    requests.post("esp ip address",data = (x_time,y_time))
-
-    return render_template("home.html")
+    x_time = FIELD_X_SIZE*(x_pixel/IMG_X_SIZE)/M_PER_S
+    y_time = FIELD_Y_SIZE*(y_pixel/IMG_Y_SIZE)/M_PER_S
+    requests.post(ESP_IP,data = (x_time,y_time))
+    return redirect("localhost:5000/")
 
 
 
