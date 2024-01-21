5d3c82a337cb53d9e05c802e5bb61789631e645d
Lee Zheng <leeminz214@gmail.com>
2024-01-21 01:29:12 -0500

    RC control

--- lettuce_server/app.py
+++ lettuce_server/app.py
@@ -1,20 +1,33 @@
 from flask import Flask,render_template, request, redirect
 import requests
+from math import floor
+from time import sleep
 
 app = Flask(__name__)
 
+back_track = []
 #constants
 M_PER_S = 0.1
+S_PER_CMD = 0.5
 IMG_X_SIZE = 100
 IMG_Y_SIZE= 100
 FIELD_X_SIZE=5
 FIELD_Y_SIZE=5
-ESP_IP = "192.168.137.3"
+ESP_IP = "http://192.168.137.3"
 
 
 @app.route('/')
 def home():
     return render_template("home.html")
+
+@app.route('/return_home')
+def return_home():
+    n= len(back_track)
+    for i in range(n):
+        print(back_track)
+        cmd = list(back_track.pop())[0]
+        requests.post(ESP_IP,data= cmd)
+        sleep(0.5)
 
 
 @app.route('/new_coord', methods=['GET','POST'])
@@ -25,54 +38,56 @@ def move_to_position():
     x_time = FIELD_X_SIZE*(x_pixel/IMG_X_SIZE)/M_PER_S
     y_time = FIELD_Y_SIZE*(y_pixel/IMG_Y_SIZE)/M_PER_S
     print (x_time,y_time)
-    #requests.post(ESP_IP,data = (x_time,y_time))
+    requests.post(ESP_IP,data = f'coord:{x_time},{y_time}')
+    x_steps = floor(x_time/S_PER_CMD)
+    y_steps = floor(y_time/S_PER_CMD)
+    for i in range(x_steps):
+        back_track.append({"back":S_PER_CMD})
+    for i in range(y_steps):
+        back_track.append({"right":S_PER_CMD})
 
     return redirect("localhost:5000/")
 
 @app.route('/forward')
 def forward():
     requests.post(ESP_IP,data= "forward")
-    return 0
-
+    back_track.append({"back":S_PER_CMD})
+    
 @app.route('/back')
 def back():
     requests.post(ESP_IP,data= "back")
-    return 0
+    back_track.append({"forward":S_PER_CMD})
 
 @app.route('/left_t')
 def left_t():
     requests.post(ESP_IP,data= "left_t")
-    return 0
+    back_track.append({"right_t":S_PER_CMD})
 
 @app.route('/right_t')
 def right_t():
     requests.post(ESP_IP,data= "right_t")
-    return 0
+    back_track.append({"left_t":S_PER_CMD})
 
 @app.route('/cut')
 def cut():
     requests.post(ESP_IP,data= "cut")
-    return 0
+    
 
 @app.route('/arm_up')
 def arm_up():
     requests.post(ESP_IP,data= "arm_up")
-    return 0
 
 @app.route('/arm_down')
 def arm_down():
     requests.post(ESP_IP,data= "arm_down")
-    return 0
 
 @app.route('/arm_left')
 def arm_left():
     requests.post(ESP_IP,data= "arm_left")
-    return 0
 
 @app.route('/arm_right')
 def arm_right():
     requests.post(ESP_IP,data= "arm_right")
-    return 0
 
 
 
